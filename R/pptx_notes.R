1eeedbae23dc5a2638ef57b578adc8967b716042
muschellij2 <muschellij2@gmail.com>
2019-08-02 18:18:25 -0400

    updated but not done

--- R/pptx_notes.R
+++ R/pptx_notes.R
@@ -1,16 +1,19 @@
 #' Get Notes from XML
 #'
 #' @param file XML file from a PPTX
+#' @param collapse_text should text be collapsed by spaces?
 #'
 #' @return A character vector
 #' @export
 #'
 #' @importFrom xml2 read_xml xml_text xml_find_all
-xml_notes = function(file) {
+xml_notes = function(file, collapse_text = TRUE) {
   xdoc = xml2::read_xml(file)
   txt = xml2::xml_find_all(x = xdoc, xpath = "//a:t")
   txt = xml2::xml_text(txt)
-  txt = paste(txt, collapse = " ")
+  if (collapse_text) {
+    txt = paste(txt, collapse = " ")
+  }
   return(txt)
 }
 
@@ -28,17 +31,61 @@ xml_notes = function(file) {
 #' file = ex_file = system.file("extdata", "example.pptx",
 #' package = "ariExtra")
 #' pptx_notes(ex_file)
+#' pptx_slide_note_df(ex_file)
+#' pptx_slide_text_df(ex_file)
 pptx_notes = function(file) {
-  tdir = tempfile()
-  dir.create(tdir)
-  res = unzip(file, exdir = tdir)
-  slide_dir = file.path(tdir, "ppt", "slides")
-  slides = list.files(path = slide_dir, pattern = "[.]xml$",
-                      full.names = TRUE)
 
-  note_dir = file.path(tdir, "ppt", "notesSlides")
-  notes = list.files(path = note_dir, pattern = "[.]xml$",
-                     full.names = TRUE)
+  df = pptx_slide_note_df(file)
+  if (is.null(df)) {
+    return(NULL)
+  }
+  ss = split(df, basename(df$file))
+  res = sapply(ss, function(x) {
+    paste(x$text, collapse = " ")
+  })
+  res[ res == ""] = ";"
+  return(res)
+}
+
+#' @export
+#' @rdname pptx_notes
+pptx_slide_text_df = function(file) {
+
+  L = unzip_pptx(file)
+  slides = L$slides
+
+  if (length(slides) > 0) {
+    # in case empty notes
+    res = lapply(slides, function(x) {
+      xx = xml_notes(x, collapse_text = FALSE)
+      if (length(xx) == 0) {
+        return(NULL)
+      }
+      snum = sub("[.]xml", "", sub("slide", "", basename(x)))
+      snum = as.numeric(snum)
+      data.frame(
+        file = x,
+        slide = snum,
+        text = xx,
+        index = 1:length(xx),
+        stringsAsFactors = FALSE)
+    })
+    res = do.call(rbind, res)
+    return(res)
+  } else {
+    return(NULL)
+  }
+}
+
+#' @export
+#' @rdname pptx_notes
+pptx_slide_note_df = function(file) {
+
+  L = unzip_pptx(file)
+  notes = L$notes
+  slides = L$slides
+  note_dir = L$note_dir
+
   if (length(notes) > 0) {
     # in case empty notes
     assoc_notes = sub("slide", "", basename(slides))
@@ -49,18 +96,60 @@ pptx_notes = function(file) {
       file.create(assoc_notes[no_fe])
       notes = assoc_notes
     }
-    res = sapply(notes, xml_notes)
-    bn = basename(notes)
-    id = sub("[[:alpha:]]*(\\d.*)[.].*", "\\1", bn)
-    ord = order(as.numeric(id))
-    res = res[ord]
-    res[is.na(res)] = ""
-    bn = bn[ord]
-    names(res) = bn
-    # in case empty notes
-    res[ res == ""] = ";"
+    res = lapply(notes, function(x) {
+      xx = xml_notes(x, collapse_text = FALSE)
+      if (length(xx) == 0) {
+        xx = ""
+      }
+      snum = sub("[.]xml", "", sub("notesSlide", "", basename(x)))
+      snum = as.numeric(snum)
+      data.frame(
+        file = x,
+        slide = snum,
+        text = xx,
+        index = 1:length(xx),
+        stringsAsFactors = FALSE)
+    })
+    res = do.call(rbind, res)
     return(res)
   } else {
     return(NULL)
   }
+}
+
+
+#' @export
+#' @rdname pptx_notes
+unzip_pptx = function(file) {
+  tdir = tempfile()
+  dir.create(tdir)
+  res = unzip(file, exdir = tdir)
+  rm(res)
+  slide_dir = file.path(tdir, "ppt", "slides")
+  slides = list.files(path = slide_dir, pattern = "[.]xml$",
+                      full.names = TRUE)
+
+  note_dir = file.path(tdir, "ppt", "notesSlides")
+  notes = list.files(path = note_dir, pattern = "[.]xml$",
+                     full.names = TRUE)
+
+  tdir = normalizePath(tdir)
+  props_dir = file.path(tdir, "docProps")
+  props_file = file.path(props_dir, "core.xml")
+  ari_core_file = system.file("extdata", "docProps",
+                              "core.xml", package = "ariExtra")
+  if (!dir.exists(props_file)) {
+    dir.create(props_dir, recursive = TRUE)
+    file.copy(ari_core_file, props_file,
+              overwrite = TRUE)
+  }
+
+  L = list(slides = slides,
+           notes = notes,
+           slide_dir = slide_dir,
+           note_dir = note_dir,
+           props_dir = props_dir,
+           props_file = props_file,
+           root_dir = tdir)
+  return(L)
 }
--- R/rewrite_slides.R
+++ R/rewrite_slides.R
@@ -1,42 +1,128 @@
 #' @example file = "~/Downloads/Leanpub_MOOCs_JHU.pptx"
-rewrite_notes = function(file) {
-  library(xml2)
-  tdir = tempfile()
-  dir.create(tdir)
-  res = unzip(file, exdir = tdir)
+pptx_rewriter = function(
+  file,
+  replace_df,
+  type = c("slides", "notes")
+) {
+  L = unzip_pptx(file)
+  root_dir = L$root_dir
+  runners = L[[type]]
+  type = match.arg(type)
+  func = switch(slides = pptx_slide_text_df,
+                notes = pptx_slide_note_df)
 
-  tdir = normalizePath(tdir)
-  props_dir = file.path(tdir, "docProps")
-  props_file = file.path(props_dir, "core.xml")
-  ari_core_file = system.file("extdata", "docProps",
-                              "core.xml", package = "ariExtra")
-  if (!dir.exists(props_file)) {
-    dir.create(props_dir, recursive = TRUE)
-    file.copy(ari_core_file, props_file,
-              overwrite = TRUE)
+  df = func(file)
+  stopifnot(nrow(df) == nrow(replace_df))
+
+  run_file = runners[2]
+  slide = xml2::read_xml(run_file)
+  txt_nodes = xml2::xml_find_all(x = slide, xpath = "//a:t")
+  if (length(txt_nodes) > 0) {
+    xml2::xml_text(txt_nodes) = replace_df$text
+    xml2::write_xml(slide, run_file)
   }
 
-  slide_dir = file.path(tdir, "ppt", "slides")
-  slides = list.files(path = slide_dir, pattern = "[.]xml$",
-                      full.names = TRUE)
-
-  slide_file = slides[2]
-  slide = xml2::read_xml(slide_file)
-  txt_nodes = xml2::xml_find_all(x = slide, xpath = "//a:t")
-  txt = xml2::xml_text(txt_nodes)
-  rep_txt = sample(letters, size = length(txt))
-  paths = xml2::xml_path(txt_nodes)
-  xml_text(txt_nodes) = rep_txt
-  xml2::write_xml(slide, slide_file)
-
   owd = getwd()
-  setwd(tdir)
+  on.exit({
+    setwd(owd)
+  })
+  setwd(root_dir)
   outfile = tempfile(fileext = ".pptx")
   all_files = list.files(path = ".", recursive = TRUE,
                          all.files = TRUE, full.names = TRUE)
   utils::zip(zipfile = outfile, files = all_files)
 
-
-  setwd(owd)
   return(outfile)
 }
+
+
+# translate_pptx = function(
+#   file,
+#   target = "es",
+#   detect = TRUE,
+#   verbose = TRUE,
+#   ...) {
+#   # googleAuthR::gar_auto_auth()
+#   # googleLanguageR::gl_auth()
+#   if (!inherits(googleAuthR::Authentication$public_fields$token, "Token")) {
+#     stop("Google Language is not Authorized, see gl_auth")
+#   }
+#   text = NULL
+#   rm(list = "text")
+#
+#   if (verbose) {
+#     message("Pulling notes from Google Slides")
+#   }
+#
+#   make_bad_string = function() {
+#     x = round(runif(1, min = 1e5, max = 1000000))
+#     x = paste0(x, ";")
+#   }
+#
+#
+#   tb_df = pptx_slide_note_df(file)
+#
+#   # Actually translate
+#   L = list(id = id,
+#            table_of_changes = tb_df)
+#   if (nrow(tb_df) > 0) {
+#     bad_string =  make_bad_string()
+#     for (i in 1:10) {
+#       # just make another
+#       if (any(grepl(bad_string, tb_df$text))) {
+#         bad_string =  make_bad_string()
+#       }
+#     }
+#     stopifnot(!any(grepl(bad_string, tb_df$text)))
+#     tb_df$text = gsub("\n", bad_string,
+#                       tb_df$text)
+#
+#     tb = tb_df$text
+#     file = tempfile()
+#     writeLines(tb, con = file)
+#     if (verbose) {
+#       message("Temporary File Created: ", file,
+#               " with bad_string: ", bad_string)
+#     }
+#     if (detect) {
+#       if (verbose) {
+#         message("Detecting Language")
+#       }
+#       out = gl_detect_file(file)
+#       if (out$language == target) {
+#         message(page_id, " already in target language")
+#         return(NULL);
+#       }
+#     }
+#
+#     df = chunk_google_translate(
+#       file,
+#       target = target,
+#       chunk = TRUE,
+#       fix_header = FALSE)
+#     df$translatedText = gsub(
+#       bad_string, "\n",
+#       df$translatedText)
+#     df$text = gsub(
+#       bad_string, "\n",
+#       df$text)
+#     tb_df$text = gsub(
+#       bad_string, "\n", tb_df$text)
+#     tb = gsub(bad_string, "\n", tb)
+#
+#     tb_new = df$translatedText
+#     stopifnot(length(tb) == length(tb_new))
+#     stopifnot(!any(grepl(bad_string, tb_new)))
+#     stopifnot(!any(grepl(bad_string, tb)))
+#
+#     tb_df$text_replacement = tb_new
+#     # delete text
+#     out = pptx_rewriter(file,
+#                         tb_df)
+#     L$table_of_changes = tb_df
+#     L$outfile = out
+#   }
+#   return(L)
+# }
+
+
