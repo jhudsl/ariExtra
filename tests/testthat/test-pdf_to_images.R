ef770ccda444148229c5040cddfbbcd2cc5a51c2
muschellij2 <muschellij2@gmail.com>
2019-08-06 15:20:23 -0400

    fixed failing test

--- tests/testthat/test-pdf_to_images.R
+++ tests/testthat/test-pdf_to_images.R
@@ -6,9 +6,9 @@ testthat::test_that("PDF converted to Ari", {
 
   ex_file = system.file("extdata", "example.pdf", package = "ariExtra")
   if (file.exists(ex_file)) {
-    res = pdf_to_ari(ex_file, script = c("hey", "ho"))
+    res = pdf_to_ari(ex_file, script = c("hey", "ho"), open = FALSE)
     res = res$output_file
-    testthat::expect_length(res$output_file, 1)
+    testthat::expect_length(res, 1)
     testthat::expect_true(file.exists(res))
     testthat::expect_type(res, "character")
   }
